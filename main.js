20c732d15e00745389de56d8e2b102dcf128bff7
AlejandraMRuiz <kovydcode@gmail.com>
2020-12-02 23:28:35 -0800

    the adventure left off here

--- main.js
+++ main.js
@@ -1,4 +1,57 @@
 
+const date = document.getElementById('date');
+const category = document.getElementById('category');
+const expense = document.getElementById('expense');
+const currency = document.getElementById('currency');
+const amount = document.getElementById('amount');
+const expenseTable = document.getElementById('expense-list');
+
+
+class expense   {
+    constructor(date, category, expense, currency, amount, id)  {
+        this.date = date;
+        this.category = category;
+        this.expense = expense;
+        this.currency = currency;
+        this.amount = amount;
+        this.id = id;
+    }
+}
+
+
+function generateExpense   {
+
+    const date = document.createElement('td');
+    date.textContent = expense.date;
+    
+    const category = document.getElementById('td');
+    category.textContent = expense.category;
+
+    const expense = document.getElementById('td');
+    expense.textContent = expense.expense;
+
+    const currency = document.getElementById('td');
+    expense.textContent = expense.currency;
+
+    const amount = document.getElementById('td');
+    expense.textContent = expense.amount;
+
+    const expenseTable = document.getElementById('td');
+    
+    // const row = document.createElement('tr');
+    // const currencySymbol= this.verifyCurrencySymbol(expense);
+    // createExpense(expense, currencySymbol, tr);
+
+
+    // tr.textContent = 
+    //     <td></td>
+    //     <td></td>
+
+    // submit-btn.addEventListener('click', function(e) {
+    //     row.appendChild
+    // });
+    
+}
 
 
 
@@ -32,3 +85,7 @@
 // STEP 8: Add the type of change used [cash, credit].
 
 // STEP 9: Add a pie chart that depicts where you put your money.
+
+// STEP 10: Add a language slide button to toggle between EN & SP.
+
+// STEP 11: Make sure to call the trash, "rubbish".
