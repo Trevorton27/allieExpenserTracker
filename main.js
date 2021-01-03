e3e8c893f60e5882cbf3954363b87db01a6e3203
AlejandraMRuiz <kovydcode@gmail.com>
2021-01-02 22:48:50 -0800

    delete expenses sos

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const expenseTable = document.getElementById('table-body');
 function displayExpense(expense)   {
     const table = document.getElementById('table-body');
     const tableRow = document.createElement('tr');
-    // display date
+// display date
     const dateTableData = document.createElement('td');
     dateTableData.textContent = expense.date;
     tableRow.append(dateTableData);
@@ -36,48 +36,13 @@ function displayExpense(expense)   {
     tableRow.append(amountTableData);
     table.append(tableRow);
 
+
     // GET <tbody>
     // CREATE <tr> & <td>    
     // ADD INFO to <td>
     // APPEND <td> to <tr>
     // APPEND <tr> to <tbody>
 
-}
-
-
-function assignCurrency(expense) {
-
-    let currencySymbol = expense.currency;
-
-    switch(currencySymbol)  {
-        case 'usDollar':
-            currencySymbol = 'fas fa-dollar-sign';
-            break;
-        case 'peso':
-            currencySymbol = 'fas fa-dollar-sign';
-            break;
-        case 'pound':
-            currencySymbol = 'fas fa-pound-sign';
-            break;
-        case 'euro':
-            currencySymbol = 'fas fa-euro-sign';
-            break;
-        case 'canadianDollar':
-            currencySymbol = 'fas fa-dollar-sign';
-            break;
-        case 'japaneseYen':
-            currencySymbol = 'fas fa-yen-sign';
-            break;
-        case 'indianRupee':
-            currencySymbol = 'fas fa-rupee-sign';
-            break;
-        default:
-            currencySymbol = 'question';
-            break;
-    
-    return currencySymbol;
-    
-    }
 }
 
 
@@ -103,10 +68,39 @@ document.querySelector('form').addEventListener('submit', function(e)   {
 
 })
 
+// Delete ONE expense
+// (adapted from my todo list app)
+function deleteExpense() {
+    const deleteExpenseBtn = document.createElement("button");
+    deleteExpenseBtn.className = "deleteExpenseBtn";
+    deleteExpenseBtn.appendChild(document.createTextNode("x"));
+    deleteExpenseBtn.addEventListener("click", function(e) {       
+        // deleteSingleTaskFromLocalStorage(task.text);
+        ul.removeChild(li);
+    });
+
+    li.appendChild(deleteExpenseBtn);    
+}
 
 
 
 
+
+
+
+
+
+
+// save for later:
+
+// clearFields() {
+//     description.value = ''
+//     amount.value = ''
+//   }
+//   setDateForToday() {
+//     let today = new Date().toISOString().substr(0, 10);
+//     document.getElementById('date').value = today;
+//   }
 
 
 
@@ -118,34 +112,10 @@ document.querySelector('form').addEventListener('submit', function(e)   {
 // DONE
 
 // STEP 1: Get these inputs displaying in an HTML table.
-// in progress...
-// break this step down into parts! ! ! ! ! ! (see handwritten notes)
+// DONE
 
 // STEP 2: Ensure user can add new expenses, as in your todo app.
+// DONE
 
 // STEP 3: Ensure user can delete both individual entries and complete table.
-
-// - - - - - 
-
-// STEP 4: Get data persisting properly.
-
-// STEP 5: Create a slider (btn) for toggling between dark/light modes.
-
-// STEP 6: Add currencies you use in XE [GBP, EUR, USD, ZAR, MXN, CAD, TRY, BRL, KWD].
-
-// STEP 7: Add categories [business, personal, household, leisure, invested, other].
-// Leave merchant out. It's covered in expense description if user wants to.
-
-// STEP 8: Add the type of change used [cash, credit]. Actually, DON'T. Just have it be
-// 'Method of Payment' and allow user to create these into a drop list. Cuz I'd personally
-// want to combine both my bank and MOP, for example: WF CC / HSBC Debit / US AMEX.
-
-// STEP 9: Add a pie chart that depicts where you put your money.
-
-// STEP 10: Add a language slide button to toggle between EN & SP.
-
-// STEP 11: Make sure to call the trash, "rubbish".
-
-// STEP 12: Add to the copyright "with minimal help from Amitay" (or similar).
-
-// STEP 13: Include one extra column that displays a country flag icon to left of currencySymbol.
+// in progress...
